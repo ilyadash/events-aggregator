dae8eb411b33356a122eb596f309cc716ec56f7d
Ilya Dashkov <daschkov.id15@physics.msu.ru>
2026-07-18 19:05:57 +0300

    Added service synv before uvicorn server starts

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
 uv run alembic upgrade head
+uv run python -c "import asyncio; from app.services.sync_service import sync_all; asyncio.run(sync_all())"
 uv run uvicorn app.main:app --host 0.0.0.0 --port 8000
